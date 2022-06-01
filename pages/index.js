4003a76015231339de0d18e9fc15556ad0a1c2d8
SadikHasanRafi <sadikkhasan@gmail.com>
2022-06-02 01:34:27 +0600

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,5 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello !</h1>;
 }
